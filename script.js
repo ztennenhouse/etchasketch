8264398a2b340421903b8db5d209a89e77d584b4
ztennenhouse <zthouse20@gmail.com>
2022-01-05 10:38:56 -0800

    First playable draft of etch-a-sketch

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-var div = document.createElement("div");
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.setAttribute('id', 'box' + c);
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "square";
+  };
+};
+
+makeRows(16, 16);
+
+document.querySelectorAll(".square").forEach(item => {
+item.addEventListener('mouseover', function() {
+    item.classList.replace('square', 'black');
+
+})
+})
